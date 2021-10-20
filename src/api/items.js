a9e2ef5962f21f4e2ed6c8366b7928c0374040be
innovationmgt <87521115+innovationmgt@users.noreply.github.com>
2021-10-20 11:58:38 +0800

    Update items.js

--- src/api/items.js
+++ src/api/items.js
@@ -1,20 +1,20 @@
 export default [
     {
       id: 1,
-      title: 'Shenmue',
-      price: 50,
-      category: 'Dreamcast',
+      title: 'LEGO Star Wars 75250 Pasaana Speeder Chase Vehicle Building Kit (373 Pieces)',
+      price: 154.84,
+      category: 'Toys',
     },
     {
       id: 2,
       title: 'Sonic Adventure',
       price: 25,
-      category: 'Dreamcast',
+      category: 'Toys',
     },
     {
       id: 3,
       title: 'Soul Calibur',
       price: 30,
-      category: 'Dreamcast',
+      category: 'Toys',
     },
   ];
