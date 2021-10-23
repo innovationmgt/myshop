e26eabd1f98b210f577e1c6ed1e3323874da7481
innovationmgt <87521115+innovationmgt@users.noreply.github.com>
2021-10-23 13:06:17 +0800

    Update items.js

--- src/api/items.js
+++ src/api/items.js
@@ -3,18 +3,18 @@ export default [
       id: 1,
       title: 'LEGO Star Wars 75250 Pasaana Speeder Chase Vehicle Building Kit (373 Pieces)',
       price: 58.40,
-      category: 'Toys',
+      category: 'Toy',
     },
     {
       id: 2,
       title: 'LEGO Star Wars 75271 Luke Skywalker's Landspeeder Building Kit (236 Pieces)',
       price: 86.90,
-      category: 'Toys',
+      category: 'Toy',
     },
     {
       id: 3,
       title: 'LEGO 75288 Star Wars TM AT-AT™',
       price: 237.90,
-      category: 'Toys',
+      category: 'Toy',
     },
   ];
